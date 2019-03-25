e66db646df804268ce07dcd1647a2b6a1da863d1
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-26 07:43:12 +1100

    Added some types

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { roleBuilder } from "roles/role.builder";
 import { tower } from "tower";
 
 export const loop: any = function () {
-    var towers = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER, my: true } });
+    let towers: AnyStructure[] = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER, my: true } });
     towers.forEach(tower.run);
 
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
+    for (let name in Game.creeps) {
+        let creep: Creep = Game.creeps[name];
 
         if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
