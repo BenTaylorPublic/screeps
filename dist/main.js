285488857b3bb6d472ea1fb506b2ab0e5aafe02b
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-26 20:06:32 +1100

    Release v1.0.0

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const rcl1_role_all_1 = require("rcl1.role.all");
 const tower_1 = require("tower");
 let globalId = 0;
+console.log("Starting script v1.0.0");
 exports.loop = function () {
     let towers = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER, my: true } });
     towers.forEach(tower_1.tower.run);
