b442a918fe710d9fc13727d64221afc68cbc0c41
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-26 20:28:58 +1100

    Added logic to force updating the room controller if it goes below 5000

--- src/rcl1.role.all.ts
+++ src/rcl1.role.all.ts
@@ -11,37 +11,45 @@ export const rcl1RoleAll: any = {
 
         if (creep.memory.harvesting) {
             //Harvesting
-            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
             if (source && creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
         } else {
             //Not harvesting
-            //Adding energy to structures that need it
-            var structureToAddTo = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure: any) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            //Check if controller is anywhere close to downgrading
+            let forceUpdateController: boolean = false;
+            if (creep.room.controller != null &&
+                creep.room.controller.ticksToDowngrade < 5000) {
+                forceUpdateController = true;
+            }
+            if (!forceUpdateController) {
+                //Adding energy to structures that need it
+                let structureToAddTo = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (structure: any) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+                });
+                if (structureToAddTo) {
+                    if (creep.transfer(structureToAddTo, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(structureToAddTo);
+                        return; //Don't need to look at other things to do
+                    }
                 }
-            });
-            if (structureToAddTo) {
-                if (creep.transfer(structureToAddTo, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structureToAddTo);
-                    return; //Don't need to look at other things to do
+
+                //Building construction sites
+                let constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
+                if (constructionSites.length) {
+                    if (creep.build(constructionSites[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(constructionSites[0]);
+                        return; //Don't need to look at other things to do
+                    }
                 }
             }
 
-            //Building construction sites
-            var constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if (constructionSites.length) {
-                if (creep.build(constructionSites[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(constructionSites[0]);
-                    return; //Don't need to look at other things to do
-                }
-            }
-
-            //Upgrading
+            //Upgrading room controller
             if (creep.upgradeController(creep.room.controller as StructureController) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller as StructureController);
                 return; //Don't need to look at other things to do
