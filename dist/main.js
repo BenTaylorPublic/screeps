88d2c759a7d50ac79426a8fabc21423913f21743
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-26 18:48:17 +1100

    Release

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const role_harvester_1 = require("role.harvester");
-const role_upgrader_1 = require("role.upgrader");
-const role_builder_1 = require("role.builder");
+const rcl1_role_all_1 = require("rcl1.role.all");
 const tower_1 = require("tower");
 let globalId = 0;
 exports.loop = function () {
     let towers = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER, my: true } });
     towers.forEach(tower_1.tower.run);
-    let harvesterCount = 0;
-    let upgraderCount = 0;
-    let builderCount = 0;
+    let creepCount = 0;
+    //Clear all dead creeps
     for (var i in Memory.creeps) {
         if (!Game.creeps[i]) {
             delete Memory.creeps[i];
@@ -18,45 +15,16 @@ exports.loop = function () {
     }
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        if (creep.memory.role === "harvester") {
-            role_harvester_1.roleHarvester.run(creep);
-            harvesterCount++;
-        }
-        else if (creep.memory.role === "upgrader") {
-            role_upgrader_1.roleUpgrader.run(creep);
-            upgraderCount++;
-        }
-        else if (creep.memory.role === "builder") {
-            role_builder_1.roleBuilder.run(creep);
-            builderCount++;
-        }
+        rcl1_role_all_1.rcl1RoleAll.run(creep);
     }
-    if (harvesterCount < 2) {
+    if (creepCount < 6) {
         let newCreep = spawnHarvester(Game.spawns.Spawn1);
-        role_harvester_1.roleHarvester.run(newCreep);
-    }
-    else if (upgraderCount < 2) {
-        let newCreep = spawnUpgrader(Game.spawns.Spawn1);
-        role_upgrader_1.roleUpgrader.run(newCreep);
-    }
-    else if (builderCount < 2) {
-        let newCreep = spawnBuilder(Game.spawns.Spawn1);
-        role_builder_1.roleBuilder.run(newCreep);
+        rcl1_role_all_1.rcl1RoleAll.run(newCreep);
     }
 };
 function spawnHarvester(spawn) {
     let id = getId();
-    spawn.spawnCreep([MOVE, CARRY, WORK], "Creep" + id, { memory: { role: "harvester" } });
-    return Game.creeps["Creep" + id];
-}
-function spawnUpgrader(spawn) {
-    let id = getId();
-    spawn.spawnCreep([MOVE, CARRY, WORK], "Creep" + id, { memory: { role: "upgrader" } });
-    return Game.creeps["Creep" + id];
-}
-function spawnBuilder(spawn) {
-    let id = getId();
-    spawn.spawnCreep([MOVE, CARRY, WORK], "Creep" + id, { memory: { role: "builder" } });
+    spawn.spawnCreep([MOVE, CARRY, WORK], "Creep" + id, { memory: { role: "rcl1" } });
     return Game.creeps["Creep" + id];
 }
 function getId() {
--- dist/rcl1.role.all.js
+++ dist/rcl1.role.all.js
@@ -2,32 +2,50 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.rcl1RoleAll = {
     run: function (creep) {
-        if (creep.memory.unloading && creep.carry.energy == 0) {
-            creep.memory.unloading = false;
+        //Swapping state
+        if (!creep.memory.harvesting && creep.carry.energy == 0) {
+            creep.memory.harvesting = true;
             creep.say("harvesting");
         }
-        else if (!creep.memory.unloading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.unloading = true;
-            creep.say("unloading");
+        else if (creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.harvesting = false;
+            creep.say("not harvesting");
         }
-        if (!creep.memory.unloading) {
+        if (creep.memory.harvesting) {
+            //Harvesting
             var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
             if (source && creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
         }
         else {
-            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            //Not harvesting
+            //Adding energy to structures that need it
+            var structureToAddTo = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
                         structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
             });
-            if (target) {
-                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
+            if (structureToAddTo) {
+                if (creep.transfer(structureToAddTo, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(structureToAddTo);
+                    return; //Don't need to look at other things to do
                 }
+            }
+            //Building construction sites
+            var constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if (constructionSites.length) {
+                if (creep.build(constructionSites[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(constructionSites[0]);
+                    return; //Don't need to look at other things to do
+                }
+            }
+            //Upgrading
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+                return; //Don't need to look at other things to do
             }
         }
     }
