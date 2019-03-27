bc0ca778e9ec93e6e872d6c5a5a107f2abe3ddee
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-27 12:09:53 +1100

    Put in the logic to place source container caches

--- src/stage2.controller.ts
+++ src/stage2.controller.ts
@@ -1,43 +1,79 @@
 export const stage2Controller: any = {
-    run: function (room: RoomWithAssignedData) {
-        if (Game.rooms[room.name] == null) {
+    run: function (myRoom: RoomWithAssignedData) {
+
+        if (Game.rooms[myRoom.name] == null) {
             //No longer have vision of this room
-            console.log("No longer have vision of room " + room.name);
+            console.log("No longer have vision of room " + myRoom.name);
             for (let i = 0; i < Memory.myMemory.rooms.length; i++) {
-                if (Memory.myMemory.rooms[i].name === room.name) {
+                if (Memory.myMemory.rooms[i].name === myRoom.name) {
                     delete Memory.myMemory.rooms[i];
                     return;
                 }
             }
-            console.log("Couldn't delete the room reference from myMemory")
+            console.log("Couldn't delete the room reference from myMemory");
             return;
         }
+
+        const room: Room = Game.rooms[myRoom.name];
+
         //Can still see the room
         //Check if containers are setup
-        if (room.containers.length < room.sources.length) {
+        if (myRoom.containers.length < myRoom.sources.length) {
             //Containers aren't set up
-            //TODO:
-            /*
-            If a source has no assigned containers
-            Build one, 2 blocks away
-            Using room.getTerrain().get(x,y)
-            Make sure it's not TERRAIN_MASK_WALL
-            Has to be in one of these:
-            x - 1, y + 2
-            x    , y + 2
-            x + 1, y + 2
-            x - 2, y + 1
-            x + 2, y + 1
-            x - 2, y
-            x + 2, y
-            x - 2, y - 1
-            x + 2, y - 1
-            x - 1, y - 2
-            x    , y - 2
-            x + 1, y - 2
-            If all of them are walls, throw an error, because the source shouldn't be accessible
-            Otherwise, add it to the room, and create a construction site (room.createConstructionSite(x, y, ))
-            */
+            for (let i = 0; i < myRoom.sources.length; i++) {
+                const sourceId: string = myRoom.sources[i];
+                let sourceHasContainerCache: boolean = false;
+                for (let j = 0; j < myRoom.containers.length; j++) {
+                    const container: ContainerWithAssignedData = myRoom.containers[j];
+                    if (container.role === 0 && container.assignedSource === sourceId) {
+                        sourceHasContainerCache = true;
+                        break; //Break this innerloop
+                    }
+                }
+                if (!sourceHasContainerCache) {
+                    //No container cache
+                    const source: Source = Game.getObjectById<Source>(sourceId) as Source;
+                    if (source == null) {
+                        console.error("Couldn't get a source with ID " + sourceId);
+                        continue;
+                    }
+                    const sourcePosX: number = source.pos.x;
+                    const sourcePosY: number = source.pos.y;
+                    const terrain: RoomTerrain = room.getTerrain();
+                    if (terrain.get(sourcePosX - 1, sourcePosY + 2) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX - 1, sourcePosY + 2);
+                    } else if (terrain.get(sourcePosX, sourcePosY + 2) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX, sourcePosY + 2);
+                    } else if (terrain.get(sourcePosX + 1, sourcePosY + 2) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX + 1, sourcePosY + 2);
+                    } else if (terrain.get(sourcePosX - 2, sourcePosY + 1) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX - 2, sourcePosY + 1);
+                    } else if (terrain.get(sourcePosX + 2, sourcePosY + 1) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX + 2, sourcePosY + 1);
+                    } else if (terrain.get(sourcePosX - 2, sourcePosY) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX - 2, sourcePosY);
+                    } else if (terrain.get(sourcePosX + 2, sourcePosY) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX + 2, sourcePosY);
+                    } else if (terrain.get(sourcePosX - 2, sourcePosY - 1) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX - 2, sourcePosY - 1);
+                    } else if (terrain.get(sourcePosX + 2, sourcePosY - 1) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX + 2, sourcePosY - 1);
+                    } else if (terrain.get(sourcePosX - 1, sourcePosY - 2) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX - 1, sourcePosY - 2);
+                    } else if (terrain.get(sourcePosX, sourcePosY - 2) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX, sourcePosY - 2);
+                    } else if (terrain.get(sourcePosX + 1, sourcePosY - 2) !== TERRAIN_MASK_WALL) {
+                        placeSourceContainerCache(myRoom, sourceId, sourcePosX + 1, sourcePosY - 2);
+                    } else {
+                        console.error("Couldn't find a viable spot to place a container");
+                    }
+                }
+            }
         }
     }
 };
+
+function placeSourceContainerCache(myRoom: RoomWithAssignedData, sourceId: string, x: number, y: number): void {
+    //TODO: Code this
+    //room.createConstructionSite(x, y)
+}
