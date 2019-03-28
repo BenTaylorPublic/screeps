08a62e24e8d0c5edd76cb18f78bed838f1656a87
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-28 16:57:56 +1100

    Fixed the logic for constructing source caches

--- src/controller.logic1.ts
+++ src/controller.logic1.ts
@@ -11,54 +11,9 @@ export const controllerLogic1: any = {
 
         const room: Room = Game.rooms[myRoom.name];
 
-        //Can still see the room
-        //Check if containers are setup
-        if (myRoom.myContainers.length < myRoom.mySources.length) {
-            //Containers aren't set up
-            for (let i = 0; i < myRoom.mySources.length; i++) {
-                const mySource: MySource = myRoom.mySources[i];
-                if (mySource.cacheContainerId == null) {
-                    //No container cache
-                    const source: Source = Game.getObjectById<Source>(mySource.id) as Source;
-                    if (source == null) {
-                        console.error("Couldn't get a source with ID " + mySource.id);
-                        continue;
-                    }
-                    const sourcePosX: number = source.pos.x;
-                    const sourcePosY: number = source.pos.y;
-                    const terrain: RoomTerrain = room.getTerrain();
-                    if (terrain.get(sourcePosX - 1, sourcePosY + 2) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 1, sourcePosY + 2);
-                    } else if (terrain.get(sourcePosX, sourcePosY + 2) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX, sourcePosY + 2);
-                    } else if (terrain.get(sourcePosX + 1, sourcePosY + 2) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 1, sourcePosY + 2);
-                    } else if (terrain.get(sourcePosX - 2, sourcePosY + 1) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 2, sourcePosY + 1);
-                    } else if (terrain.get(sourcePosX + 2, sourcePosY + 1) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 2, sourcePosY + 1);
-                    } else if (terrain.get(sourcePosX - 2, sourcePosY) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 2, sourcePosY);
-                    } else if (terrain.get(sourcePosX + 2, sourcePosY) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 2, sourcePosY);
-                    } else if (terrain.get(sourcePosX - 2, sourcePosY - 1) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 2, sourcePosY - 1);
-                    } else if (terrain.get(sourcePosX + 2, sourcePosY - 1) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 2, sourcePosY - 1);
-                    } else if (terrain.get(sourcePosX - 1, sourcePosY - 2) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 1, sourcePosY - 2);
-                    } else if (terrain.get(sourcePosX, sourcePosY - 2) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX, sourcePosY - 2);
-                    } else if (terrain.get(sourcePosX + 1, sourcePosY - 2) !== TERRAIN_MASK_WALL) {
-                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 1, sourcePosY - 2);
-                    } else {
-                        console.error("Couldn't find a viable spot to place a container");
-                    }
-                }
-            }
-        }
 
-        //TODO: Check if there's a bank
+        //Can still see the room
+        ensureTheRoomIsSetup(myRoom);
 
         let creepCount = 0;
         for (let i = 0; i < myRoom.myCreeps.length; i++) {
@@ -81,13 +36,100 @@ export const controllerLogic1: any = {
     }
 };
 
+function ensureTheRoomIsSetup(myRoom: MyRoom): void {
+    //Check if containers are setup
+    if (myRoom.myContainers.length <= myRoom.mySources.length) {
+        //Containers aren't set up
+        ensureTheCachesAreSetup(myRoom);
+    }
+
+    //TODO: Check if there's a bank
+}
+
+function ensureTheCachesAreSetup(myRoom: MyRoom) {
+    const room: Room = Game.rooms[myRoom.name];
+
+    for (let i = 0; i < myRoom.mySources.length; i++) {
+        const mySource: MySource = myRoom.mySources[i];
+        if (mySource.cacheContainerId == null) {
+            //No container cache
+            const source: Source = Game.getObjectById<Source>(mySource.id) as Source;
+            if (source == null) {
+                console.error("Couldn't get a source with ID " + mySource.id);
+                continue;
+            }
+            const sourcePosX: number = source.pos.x;
+            const sourcePosY: number = source.pos.y;
+            const terrain: RoomTerrain = room.getTerrain();
+
+            if (isNotWall(terrain, sourcePosX, sourcePosY + 1) &&
+                isNotWall(terrain, sourcePosX, sourcePosY + 2)) { //TM
+                placeSourceContainerCache(myRoom, mySource, sourcePosX, sourcePosY + 2);
+            } else if (isNotWall(terrain, sourcePosX - 1, sourcePosY) &&
+                isNotWall(terrain, sourcePosX - 2, sourcePosY)) { //ML
+                placeSourceContainerCache(myRoom, mySource, sourcePosX - 2, sourcePosY);
+            } else if (isNotWall(terrain, sourcePosX + 1, sourcePosY) &&
+                isNotWall(terrain, sourcePosX + 2, sourcePosY)) { //MR
+                placeSourceContainerCache(myRoom, mySource, sourcePosX + 2, sourcePosY);
+            } else if (isNotWall(terrain, sourcePosX, sourcePosY - 1)
+                && isNotWall(terrain, sourcePosX, sourcePosY - 2)) { //BM
+                placeSourceContainerCache(myRoom, mySource, sourcePosX, sourcePosY - 2);
+            } else {
+                if (isNotWall(terrain, sourcePosX - 1, sourcePosY + 1)) { //TL
+                    if (isNotWall(terrain, sourcePosX - 1, sourcePosY + 2)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 1, sourcePosY + 2);
+                        continue;
+                    } else if (isNotWall(terrain, sourcePosX - 2, sourcePosY + 1)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 2, sourcePosY + 1);
+                        continue;
+                    }
+                }
+                if (isNotWall(terrain, sourcePosX + 1, sourcePosY + 1)) { //TR
+                    if (isNotWall(terrain, sourcePosX + 1, sourcePosY + 2)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 1, sourcePosY + 2);
+                        continue;
+                    } else if (isNotWall(terrain, sourcePosX + 2, sourcePosY + 1)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 2, sourcePosY + 1);
+                        continue;
+                    }
+                }
+                if (isNotWall(terrain, sourcePosX - 1, sourcePosY - 1)) { //BL
+                    if (isNotWall(terrain, sourcePosX - 2, sourcePosY - 1)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 2, sourcePosY - 1);
+                        continue;
+                    } else if (isNotWall(terrain, sourcePosX - 1, sourcePosY - 2)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX - 1, sourcePosY - 2);
+                        continue;
+                    }
+                }
+
+                if (isNotWall(terrain, sourcePosX + 1, sourcePosY - 1)) { //BR
+                    if (isNotWall(terrain, sourcePosX + 1, sourcePosY - 2)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 1, sourcePosY - 2);
+                        continue;
+                    } else if (isNotWall(terrain, sourcePosX + 2, sourcePosY - 1)) {
+                        placeSourceContainerCache(myRoom, mySource, sourcePosX + 2, sourcePosY - 1);
+                        continue;
+                    }
+                }
+
+                console.error("Couldn't find a viable spot to place a container");
+            }
+        }
+    }
+}
+
 function placeSourceContainerCache(myRoom: MyRoom, mySource: MySource, x: number, y: number): void {
     //TODO: Code this
-    // console.log("Placing source container cache at " + x.toString() + ", " + y.toString());
+    console.log("Placing source container cache at " + x.toString() + ", " + y.toString());
     //room.createConstructionSite(x, y)
     //Set mySource.cacheContainerId
     //Set myContainer.assignedSourceId
 
+}
+
+function isNotWall(terrain: RoomTerrain, x: number, y: number): boolean {
+    return terrain.get(x, y) !== TERRAIN_MASK_WALL;
 }
 
 function spawnBasicWorker(spawn: StructureSpawn): Creep {
