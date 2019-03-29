febd0037e6c2a6fef6eb8fda6c7a0faa086c7f27
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-29 18:13:23 +1100

    Added the logic to control miners

--- src/role.miner.ts
+++ src/role.miner.ts
@@ -1,5 +1,34 @@
 export const roleMiner: any = {
     run: function (miner: Miner) {
+        if (miner.cacheContainerIdToPutIn == null) {
+            console.log("Miner hasn't been given a cache container id. Creep ID: " + miner.name);
+            return;
+        }
+        const cache: StructureContainer | null = Game.getObjectById<StructureContainer>(miner.cacheContainerIdToPutIn);
+        if (cache == null) {
+            console.log("Miner has been given a cache which is null. Creep ID: " + miner.name);
+            return;
+        }
+
+        const creep: Creep = Game.creeps[miner.name];
+        if (creep == null) {
+            console.log("Miner creep is null. Creep ID: " + miner.name);
+            return;
+        }
+
+        if (cache.pos.isEqualTo(creep.pos)) {
+            //In location
+            const source: Source | null = Game.getObjectById<Source>(miner.sourceId);
+            if (source == null) {
+                console.log("Miner has been given a source which is null. Creep ID: " + miner.name);
+                return;
+            }
+
+            creep.harvest(source);
+        } else {
+            //Move to cache
+            creep.moveTo(cache);
+        }
 
     }
 };
--- src/role.minerAndWorker.ts
+++ src/role.minerAndWorker.ts
@@ -15,7 +15,7 @@ export const roleMinerAndWorker: any = {
 
         if (minerAndWorker.mining === true) {
             //mining
-            const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            const source: Source | null = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
             if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
@@ -33,14 +33,14 @@ export const roleMinerAndWorker: any = {
             }
             if (!givenCommand) {
                 //Adding energy to structures that need it
-                const structureToAddTo = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                const structureToAddTo: Structure | null = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (structure: any) => {
                         return (structure.structureType === STRUCTURE_EXTENSION ||
                             structure.structureType === STRUCTURE_SPAWN ||
                             structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                     }
                 });
-                if (structureToAddTo) {
+                if (structureToAddTo != null) {
                     if (creep.transfer(structureToAddTo, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(structureToAddTo);
                         givenCommand = true;
@@ -50,7 +50,8 @@ export const roleMinerAndWorker: any = {
 
             //Building construction sites
             if (!givenCommand) {
-                const constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
+                const constructionSites: ConstructionSite<BuildableStructureConstant>[]
+                    = creep.room.find(FIND_CONSTRUCTION_SITES);
                 if (constructionSites.length) {
                     if (creep.build(constructionSites[0]) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(constructionSites[0]);
