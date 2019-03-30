a617a83053aea608b2b9568dcfcc71cd69f9b4e5
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-30 19:23:46 +1100

    Writing down logic for room stages

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,6 +16,18 @@ interface MyRoom {
     spawnName?: string;
     mySources: MySource[];
     myContainers: MyContainer[];
+    /*
+        Loosely based on RCL
+        0: RCL 0
+        1: RCL 1, fixed amount of MinerAndWorkers
+        1.20: RCL 2, Using MinerAndWorker creeps to build source caches and bank
+        1.40: Creating Miners and haulers
+        1.60: Miners and haulers are fully assigned
+        2: Start spawning builders when bank hits cap, only spawn MinerAndWorker on panic mode
+        2.5: Start constructing tower
+        3: Tower complete, Haulers place road when they walk
+     */
+    roomStage: number;
 }
 
 interface MySource {
