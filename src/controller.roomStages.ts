fb03563962fb5e13726d1eb668330354e44f311e
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-31 09:22:46 +1100

    Reworked room stages switching logic

--- src/controller.roomStages.ts
+++ src/controller.roomStages.ts
@@ -33,12 +33,34 @@ export const controllerRoomStages: any = {
 
         if (myRoom.roomStage === 1) {
             if (room.controller.level === 2) {
-                myRoom.roomStage = 1.2;
-                console.log("Room " + myRoom.name + " advanced to room stage 1.2");
+                myRoom.roomStage = 2;
+                console.log("Room " + myRoom.name + " advanced to room stage 2");
             }
         }
 
-        if (myRoom.roomStage === 1.2) {
+        if (myRoom.roomStage === 2) {
+            if (room.controller.level === 3) {
+                myRoom.roomStage = 2.2;
+                console.log("Room " + myRoom.name + " advanced to room stage 2.2");
+            }
+        }
+
+        if (myRoom.roomStage === 2.2) {
+            const towers: StructureTower[] = room.find<StructureTower>(FIND_STRUCTURES, {
+                filter: (structure: Structure) => {
+                    return structure.structureType === STRUCTURE_TOWER;
+                }
+            });
+
+            if (towers.length === 1) {
+                //Tower has been built
+                myRoom.roomStage = 2.4;
+                console.log("Room " + myRoom.name + " advanced to room stage 2.4");
+            }
+        }
+
+        if (myRoom.roomStage === 2.4) {
+
             const containers: StructureContainer[] = room.find<StructureContainer>(FIND_STRUCTURES, {
                 filter: (structure: Structure) => {
                     return structure.structureType === STRUCTURE_CONTAINER;
@@ -47,12 +69,13 @@ export const controllerRoomStages: any = {
 
             if (containers.length === room.find(FIND_SOURCES_ACTIVE).length + 1) {
                 //Caches and bank must be built
-                myRoom.roomStage = 1.4;
-                console.log("Room " + myRoom.name + " advanced to room stage 1.4");
+                myRoom.roomStage = 2.6;
+                console.log("Room " + myRoom.name + " advanced to room stage 2.6");
             }
+
         }
 
-        if (myRoom.roomStage === 1.4) {
+        if (myRoom.roomStage === 2.6) {
             let haulerAmount: number = 0;
             let minerAmount: number = 0;
             for (let i = 0; i < myRoom.myCreeps.length; i++) {
@@ -66,12 +89,14 @@ export const controllerRoomStages: any = {
             const amountOfSources: number = room.find(FIND_SOURCES_ACTIVE).length;
             if (haulerAmount === amountOfSources &&
                 minerAmount === amountOfSources) {
-                myRoom.roomStage = 1.6;
-                console.log("Room " + myRoom.name + " advanced to room stage 1.6");
+                myRoom.roomStage = 2.8;
+                console.log("Room " + myRoom.name + " advanced to room stage 2.8");
             }
+
         }
 
-        if (myRoom.roomStage === 1.6) {
+        if (myRoom.roomStage === 2.8) {
+
             let myBankContainer: MyContainer | null = null;
 
             for (let i = 0; i < myRoom.myContainers.length; i++) {
@@ -87,33 +112,26 @@ export const controllerRoomStages: any = {
                 if (bankContainer != null) {
                     if (bankContainer.store[RESOURCE_ENERGY] === bankContainer.storeCapacity) {
                         //Bank is full
-                        myRoom.roomStage = 2;
-                        console.log("Room " + myRoom.name + " advanced to room stage 2");
+                        myRoom.roomStage = 3;
+                        console.log("Room " + myRoom.name + " advanced to room stage 3");
                     }
                 }
             }
+
         }
 
-        if (myRoom.roomStage === 2) {
-            if (room.controller.level === 3) {
-                myRoom.roomStage = 2.5;
-                console.log("Room " + myRoom.name + " advanced to room stage 2.5");
-            }
-        }
 
-        if (myRoom.roomStage === 2.5) {
-
-            const towers: StructureTower[] = room.find<StructureTower>(FIND_STRUCTURES, {
-                filter: (structure: Structure) => {
-                    return structure.structureType === STRUCTURE_TOWER;
-                }
-            });
-
-            if (towers.length === 1) {
-                //Tower has been built
-                myRoom.roomStage = 3;
-                console.log("Room " + myRoom.name + " advanced to room stage 3");
-            }
+        if (myRoom.roomStage === 3) {
+            //TODO:
         }
     }
 };
+
+
+
+
+
+
+
+
+
