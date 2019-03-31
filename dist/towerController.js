43868f00de43aeb5a6544a3ee3e9287d9257c40a
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-03-31 11:37:42 +1100

    Repair first because I'm in safe mode

--- dist/towerController.js
+++ dist/towerController.js
@@ -2,34 +2,47 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.towerController = {
     run: function (tower) {
-        const hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
-        let target = null;
-        for (let i = 0; i < hostiles.length; i++) {
-            const creep = hostiles[i];
-            let healer = false;
-            for (let j = 0; j < creep.body.length; j++) {
-                const bodyPart = creep.body[j];
-                if (bodyPart.type === HEAL) {
-                    healer = true;
-                    break;
+        const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure != null) {
+            tower.repair(closestDamagedStructure);
+            return;
+        }
+        const hostileCreeps = tower.room.find(FIND_HOSTILE_CREEPS);
+        if (hostileCreeps.length >= 1) {
+            const allyList = ["mooseyman", "nimphious"];
+            for (let i = hostileCreeps.length - 1; i >= 0; i--) {
+                const possibleHostileCreep = hostileCreeps[i];
+                for (let j = 0; j < allyList.length; j++) {
+                    if (possibleHostileCreep.owner.username.toLowerCase() === allyList[j]) {
+                        hostileCreeps.slice(i, 1);
+                        break;
+                    }
                 }
             }
-            if (healer) {
-                target = creep;
-                break;
+            let target = null;
+            for (let i = 0; i < hostileCreeps.length; i++) {
+                const hostileCreep = hostileCreeps[i];
+                let healer = false;
+                for (let j = 0; j < hostileCreep.body.length; j++) {
+                    const bodyPart = hostileCreep.body[j];
+                    if (bodyPart.type === HEAL) {
+                        healer = true;
+                        break;
+                    }
+                }
+                if (healer) {
+                    target = hostileCreep;
+                    break;
+                }
+                else if (target == null) {
+                    target = hostileCreep;
+                }
             }
-        }
-        if (target != null &&
-            target.owner.username.toLowerCase() !== "mooseyman" &&
-            target.owner.username.toLowerCase() !== "nimphious") {
-            tower.attack(target);
-        }
-        else {
-            const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if (closestDamagedStructure != null) {
-                tower.repair(closestDamagedStructure);
+            if (target != null) {
+                tower.attack(target);
+                return;
             }
         }
     }
--- src/towerController.ts
+++ src/towerController.ts
@@ -2,35 +2,53 @@
 export const towerController: any = {
     run: function (tower: StructureTower) {
 
-        const hostiles: Creep[] = tower.room.find(FIND_HOSTILE_CREEPS);
-        let target: Creep | null = null;
-        for (let i = 0; i < hostiles.length; i++) {
-            const creep: Creep = hostiles[i];
-            let healer: boolean = false;
-            for (let j = 0; j < creep.body.length; j++) {
-                const bodyPart: BodyPartDefinition = creep.body[j];
-                if (bodyPart.type === HEAL) {
-                    healer = true;
-                    break;
+
+        const closestDamagedStructure: Structure | null = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure != null) {
+            tower.repair(closestDamagedStructure);
+            return;
+        }
+
+        const hostileCreeps: Creep[] = tower.room.find(FIND_HOSTILE_CREEPS);
+        if (hostileCreeps.length >= 1) {
+            const allyList: string[] = ["mooseyman", "nimphious"];
+
+            for (let i = hostileCreeps.length - 1; i >= 0; i--) {
+                const possibleHostileCreep: Creep = hostileCreeps[i];
+                for (let j = 0; j < allyList.length; j++) {
+                    if (possibleHostileCreep.owner.username.toLowerCase() === allyList[j]) {
+                        hostileCreeps.slice(i, 1);
+                        break;
+                    }
                 }
             }
 
-            if (healer) {
-                target = creep;
-                break;
+            let target: Creep | null = null;
+            for (let i = 0; i < hostileCreeps.length; i++) {
+                const hostileCreep: Creep = hostileCreeps[i];
+                let healer: boolean = false;
+                for (let j = 0; j < hostileCreep.body.length; j++) {
+                    const bodyPart: BodyPartDefinition = hostileCreep.body[j];
+                    if (bodyPart.type === HEAL) {
+                        healer = true;
+                        break;
+                    }
+                }
+
+                if (healer) {
+                    target = hostileCreep;
+                    break;
+                } else if (target == null) {
+                    target = hostileCreep;
+                }
+            }
+            if (target != null) {
+                tower.attack(target);
+                return;
             }
         }
-        if (target != null &&
-            target.owner.username.toLowerCase() !== "mooseyman" &&
-            target.owner.username.toLowerCase() !== "nimphious") {
-            tower.attack(target);
-        } else {
-            const closestDamagedStructure: Structure | null = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if (closestDamagedStructure != null) {
-                tower.repair(closestDamagedStructure);
-            }
-        }
+
     }
 };
