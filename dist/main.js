5034ab169997ecc4d8591a6c4067fb1ba5f87099
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-04 18:07:10 +1100

    Release

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,31 @@ const memory_controller_1 = require("memory.controller");
 console.log("Script reloaded");
 setupMyMemory();
 const room = Memory.myMemory.myRooms[0];
+room.myCreeps = [];
+for (let i = 0; i < room.mySources.length; i++) {
+    const mySource = room.mySources[i];
+    mySource.haulerNames = [];
+    if (mySource.id === "5bbcaa719099fc012e6315f9") {
+        mySource.cachePos = {
+            x: 5,
+            y: 46,
+            roomName: room.name
+        };
+    }
+    else if (mySource.id === "5bbcaa719099fc012e6315f7") {
+        mySource.cachePos = {
+            x: 19,
+            y: 3,
+            roomName: room.name
+        };
+    }
+}
+room.bankPos = {
+    x: 17,
+    y: 26,
+    roomName: room.name
+};
+delete room.myContainers;
 for (let i = 0; i < room.mySources.length; i++) {
     const element = room.mySources[i];
     element.haulerNames = [];
