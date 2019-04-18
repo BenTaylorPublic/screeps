30f4ef38aa50d8d3555235369c73a04fadbb29d1
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-18 17:51:27 +1000

    Fuck it 6 was okay

--- dist/constants.js
+++ dist/constants.js
@@ -6,5 +6,5 @@ Constants.AMOUNT_OF_HAULERS_PER_SOURCE = 2;
 Constants.AMOUNT_OF_BANK_ENERGY_TO_SPAWN_LABORER = 100000;
 Constants.MAX_LABORERS = 10;
 Constants.MIN_LABORERS = 2;
-Constants.LABORERS_BEFORE_BANK = 4;
+Constants.LABORERS_BEFORE_BANK = 6;
 exports.Constants = Constants;
