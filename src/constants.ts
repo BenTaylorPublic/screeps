41d142c1cc9a280eb4fa0d359c41d524ceac8deb
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-18 17:47:48 +1000

    Setting min laborers to 4 before bank

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export class Constants {
     static AMOUNT_OF_BANK_ENERGY_TO_SPAWN_LABORER: number = 100000;
     static MAX_LABORERS: number = 10;
     static MIN_LABORERS: number = 2;
-    static LABORERS_BEFORE_BANK: number = 6;
+    static LABORERS_BEFORE_BANK: number = 4;
 }
