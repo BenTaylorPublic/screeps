383f44529a22d3e1e0883bd53351626ce77d3ef8
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-21 09:51:48 +1000

    Should have been with the last commit

--- src/live.controller.ts
+++ src/live.controller.ts
@@ -1,9 +1,7 @@
 import { liveSpawnClaimers } from "live.spawn.claimer";
-import { liveSpawnLaborers } from "live.spawn.laborer";
 
 export const liveController: any = {
     run: function (): void {
         liveSpawnClaimers.run();
-        liveSpawnLaborers.run();
     }
 };
