42275cccf0c5ab4ed62e8fa792e267cd7acd51b4
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-21 10:41:05 +1000

    Implemented 4.2 to 4.4

---
+++ src/stage.4_2.ts
@@ -0,0 +1,27 @@
+import { globalFunctions } from "global.functions";
+
+export const stage4_2: StageController = {
+    /*
+    4.2 ->  4.4 : Room has >= 2 tower
+    4.2 <-  4.4 : Room has < 2 tower
+    */
+    up: function (myRoom: MyRoom, room: Room): boolean {
+        if (globalFunctions.amountOfStructure(room, STRUCTURE_TOWER) >= 2) {
+            myRoom.roomStage = 4.4;
+            console.log("LOG: Room " + myRoom.name + " increased to room stage 4.4");
+            return true;
+        }
+        return false;
+    },
+    down: function (myRoom: MyRoom, room: Room): boolean {
+        if (globalFunctions.amountOfStructure(room, STRUCTURE_TOWER) < 2) {
+            myRoom.roomStage = 4.2;
+            console.log("LOG: Room " + myRoom.name + " decreased to room stage 4.2");
+            return true;
+        }
+        return false;
+    },
+    step: function (myRoom: MyRoom, room: Room): void {
+        globalFunctions.buildTowers(myRoom, 2);
+    }
+};
