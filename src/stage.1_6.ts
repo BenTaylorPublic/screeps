cfdd0e3f19214955081ac16aad6415f3d6df7c37
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-25 13:17:45 +1000

    Cleanup the function to place container caches

--- src/stage.1_6.ts
+++ src/stage.1_6.ts
@@ -29,51 +29,57 @@ export class Stage1_6 {
 
     private static step(myRoom: MyRoom, room: Room): void {
         const roomFlags: Flag[] = GlobalFunctions.getRoomsFlags(myRoom);
+        for (let i = roomFlags.length - 1; i >= 0; i--) {
+            const roomFlag: Flag = roomFlags[i];
+            const flagNameSplit: string[] = roomFlag.name.split("-");
+            if (flagNameSplit[0] !== "cont") {
+                roomFlags.slice(i, 1);
+            }
+        }
+
         let flagsPlaced: number = 0;
         for (let i = 0; i < roomFlags.length; i++) {
             const roomFlag: Flag = roomFlags[i];
-            const flagNameSplit: string[] = roomFlag.name.split("-");
-            if (flagNameSplit[0] === "cont") {
-                const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_CONTAINER);
-                if (result === OK) {
-                    let placedFully: boolean = false;
-                    for (let j = 0; j < myRoom.mySources.length; j++) {
-                        const mySource: MySource = myRoom.mySources[j];
-                        const source: Source | null = Game.getObjectById<Source>(mySource.id);
-                        if (source == null) {
-                            console.log("ERR: Source was null when trying to get it by ID");
-                        } else {
-                            if (source.pos.inRangeTo(roomFlag.pos, 1)) {
-                                mySource.cache = {
-                                    pos: {
-                                        x: roomFlag.pos.x,
-                                        y: roomFlag.pos.y,
-                                        roomName: myRoom.name
-                                    },
-                                    id: null
-                                };
-                                mySource.state = "Cache";
-                                flagsPlaced++;
-                                placedFully = true;
-                            } // Else it's hopefully the other source in the room...
-                        }
-                    }
-                    if (placedFully) {
-                        console.log("LOG: Placed container cache construction site");
-                        roomFlag.remove();
+            const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_CONTAINER);
+            if (result === OK) {
+                let placedFully: boolean = false;
+                for (let j = 0; j < myRoom.mySources.length; j++) {
+                    const mySource: MySource = myRoom.mySources[j];
+                    const source: Source | null = Game.getObjectById<Source>(mySource.id);
+                    if (source == null) {
+                        console.log("ERR: Source was null when trying to get it by ID");
                     } else {
-                        console.log("ERR: Placed a construction site at a flag but couldn't find a source to give it to");
+                        if (source.pos.inRangeTo(roomFlag.pos, 1)) {
+                            mySource.cache = {
+                                pos: {
+                                    x: roomFlag.pos.x,
+                                    y: roomFlag.pos.y,
+                                    roomName: myRoom.name
+                                },
+                                id: null
+                            };
+                            mySource.state = "Cache";
+                            flagsPlaced++;
+                            placedFully = true;
+                        } // Else it's hopefully the other source in the room...
                     }
-
-                } else {
-                    console.log("ERR: Placing a container cache construction site errored");
                 }
+                if (placedFully) {
+                    console.log("LOG: Placed container cache construction site");
+                    roomFlag.remove();
+                } else {
+                    console.log("ERR: Placed a construction site at a flag but couldn't find a source to give it to");
+                }
+
+            } else {
+                console.log("ERR: Placing a container cache construction site errored");
             }
         }
 
         for (let i = 0; i < myRoom.mySources.length; i++) {
             const mySource: MySource = myRoom.mySources[i];
-            if (mySource.cache != null) {
+            if (mySource.cache != null &&
+                mySource.cache.id == null) {
                 const cachePos: RoomPosition = GlobalFunctions.myPosToRoomPos(mySource.cache.pos);
                 const structures: Structure<StructureConstant>[] = cachePos.lookFor(LOOK_STRUCTURES);
                 for (let j = 0; j < structures.length; j++) {
