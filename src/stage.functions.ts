b6871571394ccddc8e2e99ae5f8415cc2c423820
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-25 13:07:01 +1000

    Cleaned up the stage functions logic

--- src/stage.functions.ts
+++ src/stage.functions.ts
@@ -4,49 +4,60 @@ export class StageFunctions {
 
     public static buildExtensions(myRoom: MyRoom, numberOfExtensionsToBuild: number): void {
         const roomFlags: Flag[] = GlobalFunctions.getRoomsFlags(myRoom);
+        for (let i = roomFlags.length - 1; i >= 0; i--) {
+            const roomFlag: Flag = roomFlags[i];
+            const flagNameSplit: string[] = roomFlag.name.split("-");
+            if (flagNameSplit[0] !== "ex") {
+                roomFlags.slice(i, 1);
+            }
+        }
         for (let i = 0; i < roomFlags.length; i++) {
             const roomFlag: Flag = roomFlags[i];
             const flagNameSplit: string[] = roomFlag.name.split("-");
-            if (flagNameSplit[0] === "ex") {
-                const extensionNumber: number = Number(flagNameSplit[1]);
-                if (extensionNumber <= numberOfExtensionsToBuild) {
-                    const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_EXTENSION);
-                    if (result === OK) {
-                        console.log("LOG: Placed extension construction site");
-                        myRoom.myExtensionPositions.push({
-                            x: roomFlag.pos.x,
-                            y: roomFlag.pos.y,
-                            roomName: myRoom.name
-                        });
-                        roomFlag.remove();
-                    } else {
-                        console.log("ERR: Placing a extension construction site errored");
-                    }
+            const extensionNumber: number = Number(flagNameSplit[1]);
+            if (extensionNumber <= numberOfExtensionsToBuild) {
+                const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_EXTENSION);
+                if (result === OK) {
+                    console.log("LOG: Placed extension construction site");
+                    myRoom.myExtensionPositions.push({
+                        x: roomFlag.pos.x,
+                        y: roomFlag.pos.y,
+                        roomName: myRoom.name
+                    });
+                    roomFlag.remove();
+                } else {
+                    console.log("ERR: Placing a extension construction site errored");
                 }
             }
+
         }
     }
 
     public static buildTowers(myRoom: MyRoom, numberOfTowersToBuild: number): void {
         const roomFlags: Flag[] = GlobalFunctions.getRoomsFlags(myRoom);
+        for (let i = roomFlags.length - 1; i >= 0; i--) {
+            const roomFlag: Flag = roomFlags[i];
+            const flagNameSplit: string[] = roomFlag.name.split("-");
+            if (flagNameSplit[0] !== "tower") {
+                roomFlags.slice(i, 1);
+            }
+        }
         for (let i = 0; i < roomFlags.length; i++) {
             const roomFlag: Flag = roomFlags[i];
             const flagNameSplit: string[] = roomFlag.name.split("-");
-            if (flagNameSplit[0] === "tower") {
-                const towerNumber: number = Number(flagNameSplit[1]);
-                if (towerNumber <= numberOfTowersToBuild) {
-                    const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_TOWER);
-                    if (result === OK) {
-                        console.log("LOG: Placed tower construction site");
-                        myRoom.myTowerPositions.push({
-                            x: roomFlag.pos.x,
-                            y: roomFlag.pos.y,
-                            roomName: myRoom.name
-                        });
-                        roomFlag.remove();
-                    } else {
-                        console.log("ERR: Placing a tower construction site errored");
-                    }
+            const towerNumber: number = Number(flagNameSplit[1]);
+            if (towerNumber <= numberOfTowersToBuild) {
+                const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_TOWER);
+                if (result === OK) {
+                    console.log("LOG: Placed tower construction site");
+                    myRoom.myTowerPositions.push({
+                        x: roomFlag.pos.x,
+                        y: roomFlag.pos.y,
+                        roomName: myRoom.name
+                    });
+                    roomFlag.remove();
+                } else {
+                    console.log("ERR: Placing a tower construction site errored");
                 }
             }
         }
