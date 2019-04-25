919058085a6cc3cbebd6215cb676a329ae898fc9
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-25 14:29:44 +1000

    Placed bank link

--- src/stage.4_6.ts
+++ src/stage.4_6.ts
@@ -27,7 +27,50 @@ export class Stage4_6 {
     }
 
     private static step(myRoom: MyRoom, room: Room): void {
+        //Source links
         StageFunctions.setupSourceLink(myRoom);
-        // TODO: Setup bank link
+
+        //Bank link logic
+        const roomFlags: Flag[] = GlobalFunctions.getRoomsFlags(myRoom);
+        for (let i = roomFlags.length - 1; i >= 0; i--) {
+            const roomFlag: Flag = roomFlags[i];
+            const flagNameSplit: string[] = roomFlag.name.split("-");
+            if (flagNameSplit[0] !== "link" ||
+                flagNameSplit[1] !== "bank") {
+                roomFlags.slice(i, 1);
+            }
+        }
+
+        let placedBankLink: boolean = false;
+
+        for (let i = 0; i < roomFlags.length; i++) {
+            const roomFlag: Flag = roomFlags[i];
+            const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(roomFlag.pos, STRUCTURE_LINK);
+            if (result === OK) {
+                myRoom.bankLink = {
+                    pos: GlobalFunctions.roomPosToMyPos(roomFlag.pos),
+                    id: null
+                };
+                roomFlag.remove();
+                placedBankLink = true;
+            } else {
+                console.log("ERR: Placing a bank link construction site errored");
+            }
+        }
+        if (myRoom.bankLink != null) {
+            const linkPos: RoomPosition = GlobalFunctions.myPosToRoomPos(myRoom.bankLink.pos);
+            const structures: Structure<StructureConstant>[] = linkPos.lookFor(LOOK_STRUCTURES);
+            for (let j = 0; j < structures.length; j++) {
+                if (structures[j].structureType === STRUCTURE_LINK) {
+                    myRoom.bankLink.id = structures[j].id;
+                    break;
+                }
+            }
+        }
+
+        if (!placedBankLink &&
+            Game.rooms[myRoom.name].find(FIND_CONSTRUCTION_SITES).length === 0) {
+            console.log("ATTENTION: Room " + myRoom.name + " needs bank link flag");
+        }
     }
 }
