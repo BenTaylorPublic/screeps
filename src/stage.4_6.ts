1dbb3f26b2abf6bb3ad456f722c06ffb11dd2463
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-04-26 10:53:35 +1000

    Ignored errors from placing source links

--- src/stage.4_6.ts
+++ src/stage.4_6.ts
@@ -54,9 +54,7 @@ export class Stage4_6 {
                 roomFlag.remove();
                 placedBankLink = true;
                 console.log("LOG: Placed a bank link construction site");
-            } else {
-                console.log("ERR: Placing a bank link construction site errored");
-            }
+            } //Don't worry about erroring
         }
         if (myRoom.bankLink != null) {
             const linkPos: RoomPosition = GlobalFunctions.myPosToRoomPos(myRoom.bankLink.pos);
