e02dd4b5a99ce42f8530068a4ed550d3b3dda5ed
Ben Taylor <benjaminjacktaylor@hotmail.com>
2019-05-07 21:10:45 +1000

    Source links will transfer to out links if they need to be topped up

--- src/room/structures/source-link.ts
+++ src/room/structures/source-link.ts
@@ -14,16 +14,36 @@ export class RoomSourceLinkController {
         }
 
         if (link.energy >= Constants.SOURCE_LINK_TRANSFER_AT) {
-            const bankLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.bankLink.id);
-            if (bankLink === null) {
-                console.log("ERR: Bank link was null when accessed by ID. Setting it to null");
-                myRoom.bankLink.id = null;
-                return;
+
+            //Need to transfer energy
+
+            let transferredToOutLink: boolean = false;
+            for (let i = 0; i < myRoom.outLinks.length; i++) {
+                const myOutLink: MyLink = myRoom.outLinks[i];
+
+                if (myOutLink.id != null) {
+                    const outLink: StructureLink | null = Game.getObjectById<StructureLink>(myOutLink.id);
+                    if (outLink != null) {
+                        if (outLink.energyCapacity - outLink.energy > link.energy) {
+                            link.transferEnergy(outLink);
+                            transferredToOutLink = true;
+                        }
+                    }
+                }
             }
 
-            //Just try it. Might error, but oh well
-            link.transferEnergy(bankLink);
-        }
-    }
+            if (!transferredToOutLink) {
+                const bankLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.bankLink.id);
+                if (bankLink === null) {
+                    console.log("ERR: Bank link was null when accessed by ID. Setting it to null");
+                    myRoom.bankLink.id = null;
+                    return;
+                }
 
+                //Just try it. Might error, but oh well
+                link.transferEnergy(bankLink);
+            }
+        }
+
+    }
 }
