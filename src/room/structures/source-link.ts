5c965b15612b20115da9c83728b65d7f08550d36
ben <benjaminjacktaylor@hotmail.com>
2019-10-19 15:18:44 +1100

    Cleaned up source link logic

--- src/room/structures/source-link.ts
+++ src/room/structures/source-link.ts
@@ -2,9 +2,12 @@ import { Constants } from "../../global/constants";
 
 export class RoomSourceLinkController {
     public static run(myRoom: MyRoom, myLink: MyLink): void {
-        if (myLink.id === null) { return; }
-        if (myRoom.bankLink == null ||
-            myRoom.bankLink.id == null) { return; }
+        if (myLink.id === null) {
+            return;
+        }
+        if (myRoom.bankLink == null || myRoom.bankLink.id == null) {
+            return;
+        }
 
         const link: StructureLink | null = Game.getObjectById<StructureLink>(myLink.id);
         if (link === null) {
@@ -17,32 +20,29 @@ export class RoomSourceLinkController {
 
             //Need to transfer energy
 
-            let transferredToOutLink: boolean = false;
             for (let i = 0; i < myRoom.outLinks.length; i++) {
                 const myOutLink: MyLink = myRoom.outLinks[i];
 
                 if (myOutLink.id != null) {
                     const outLink: StructureLink | null = Game.getObjectById<StructureLink>(myOutLink.id);
                     if (outLink != null) {
-                        if (outLink.energyCapacity - outLink.energy > link.energy) {
+                        if (outLink.energyCapacity - outLink.energy >= link.energy) {
                             link.transferEnergy(outLink);
-                            transferredToOutLink = true;
+                            return;
                         }
                     }
                 }
             }
 
-            if (!transferredToOutLink) {
-                const bankLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.bankLink.id);
-                if (bankLink === null) {
-                    console.log("ERR: Bank link was null when accessed by ID. Setting it to null");
-                    myRoom.bankLink.id = null;
-                    return;
-                }
-
-                //Just try it. Might error, but oh well
-                link.transferEnergy(bankLink);
+            const bankLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.bankLink.id);
+            if (bankLink === null) {
+                console.log("ERR: Bank link was null when accessed by ID. Setting it to null");
+                myRoom.bankLink.id = null;
+                return;
             }
+
+            //Just try it. Might error, but oh well
+            link.transferEnergy(bankLink);
         }
 
     }
