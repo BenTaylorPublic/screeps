17463fdfa20e7f006041a1775bc9e26499009075
ben <benjaminjacktaylor@hotmail.com>
2019-10-22 21:50:30 +1100

    Removed unused function

--- src/global/helper-functions.ts
+++ src/global/helper-functions.ts
@@ -58,24 +58,6 @@ export class HelperFunctions {
         return result;
     }
 
-    public static isConstructable(terrain: RoomTerrain, roomName: string, x: number, y: number): boolean {
-        // TODO: Remove terrain as a param
-        if (x < 0 || x > 49 || y < 0 || y > 49) {
-            return false;
-        }
-        if (terrain.get(x, y) !== TERRAIN_MASK_WALL) {
-            const roomPos: RoomPosition = new RoomPosition(x, y, roomName);
-            const structures: Structure<StructureConstant>[] = roomPos.lookFor(LOOK_STRUCTURES);
-            if (structures.length !== 0) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public static roomPosToMyPos(roomPos: RoomPosition): MyRoomPos {
         return {
             x: roomPos.x,
