4459e4f220348f8a345e79f691cd571fb7166a33
ben <benjaminjacktaylor@hotmail.com>
2019-10-23 20:59:04 +1100

    Sim hotfix

--- src/sim/sim-controller.ts
+++ src/sim/sim-controller.ts
@@ -16,42 +16,19 @@ export class SimController {
     //Returns true if found a target
     private static findClosestFlag(creep: Creep): boolean {
 
-        const flags: Flag[] = [];
-        const flagNames: string[] = Object.keys(Game.flags);
-        for (let i = 0; i < flagNames.length; i++) {
-            const flag: Flag = Game.flags[flagNames[i]];
-            if (flag.room != null &&
-                flag.room.name === "sim") {
-                flags.push(flag);
-            }
+        const flag: Flag = Game.flags["Flag1"];
+        const path: PathStep[] = creep.pos.findPathTo(flag.pos);
+
+        if (!path.length) {
+            return false;
         }
 
-        let closestFlag: Flag | null = null;
-        let closestFlagDistance: number = 999;
-        for (let i: number = flags.length - 1; i >= 0; i--) {
-            const path: PathStep[] = creep.pos.findPathTo(flags[i].pos);
-            if (!path.length) {
-                //No path
-                console.log("No path");
-                flags.splice(i, 1);
-            } else if (path.length < closestFlagDistance) {
-                closestFlag = flags[i];
-                closestFlagDistance = path.length;
-            }
-        }
-
-        if (closestFlag != null) {
-            this.attackTarget(creep, closestFlag);
-            return true;
-        }
-        return false;
-    }
-
-    private static attackTarget(creep: Creep, target: Flag): void {
-        if (creep.pos.inRangeTo(target.pos, 1)) {
-            console.log("ARRIVED");
+        if (creep.pos.inRangeTo(flag.pos, 0)) {
+            creep.say("ARRIVED");
         } else {
-            creep.moveTo(target);
+            creep.moveTo(flag.pos);
+            creep.say("MOVING");
         }
+        return true;
     }
 }
