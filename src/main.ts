dbaec6683ac97e46f503667a868d4da22627ff46
ben <benjaminjacktaylor@hotmail.com>
2019-11-03 21:06:38 +1100

    Added profiler to ts

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,31 @@ import {EmpireController} from "./empire/empire-controller";
 import {ReportController} from "./reporting/report-controller";
 import {ScheduleController} from "./schedule/schedule-controller";
 
+declare function require(name: string): any;
+
+const profiler: any = require("screeps-profiler");
+
 console.log("Script reloaded");
 
 setupMyMemory();
 
+profiler.enable();
 export let loop: any = function (): void {
-    const myMemory: MyMemory = Memory.myMemory;
-    MemoryController.run();
+    profiler.wrap(function (): any {
+        const myMemory: MyMemory = Memory.myMemory;
+        MemoryController.run();
 
-    ScheduleController.preLoop(myMemory);
+        ScheduleController.preLoop(myMemory);
 
-    ReportController.checkForReportFlag();
-    EmpireController.run(myMemory);
+        ReportController.checkForReportFlag();
+        EmpireController.run(myMemory);
 
-    for (let i = 0; i < myMemory.myRooms.length; i++) {
-        RoomController.run(myMemory.myRooms[i]);
-    }
+        for (let i = 0; i < myMemory.myRooms.length; i++) {
+            RoomController.run(myMemory.myRooms[i]);
+        }
 
-    MemoryController.clearBanks();
+        MemoryController.clearBanks();
+    });
 };
 
 function setupMyMemory(): void {
