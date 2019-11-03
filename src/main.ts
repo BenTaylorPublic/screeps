932cebf700c1884a1b779dcb0f88d84c2c9874d5
ben <benjaminjacktaylor@hotmail.com>
2019-11-03 21:30:24 +1100

    Attempt thirteen

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
+declare function require(name: string): any;
+
+const profiler: any = require("screeps-profiler");
+profiler.enable();
+
 import {RoomController} from "./room/room-controller";
 import {MemoryController} from "./memory/memory-controller";
 import {EmpireController} from "./empire/empire-controller";
 import {ReportController} from "./reporting/report-controller";
 import {ScheduleController} from "./schedule/schedule-controller";
 
-declare function require(name: string): any;
-
-const profiler: any = require("screeps-profiler");
-
 console.log("Script reloaded");
 
 setupMyMemory();
 
-profiler.enable();
 export let loop: any = function (): void {
     profiler.wrap(function (): any {
 
