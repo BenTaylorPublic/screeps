defd49941b7c47150a347b0e9db201bd33c69e2d
ben <benjaminjacktaylor@hotmail.com>
2019-11-04 16:12:38 +1100

    Testing profiler 2

--- src/profiler/profiler.ts
+++ src/profiler/profiler.ts
@@ -1,19 +1,15 @@
 export class Profiler {
-    public static setup(classs: DERP): void {
+    public static setup<T>(classs: T): void {
         console.log("Profiler-setup");
 
-        if (classs.prototype == null) {
-            return;
-        }
-
-        Object.getOwnPropertyNames(classs.prototype).forEach(functionName => {
-            console.log(functionName);
+        Object.getOwnPropertyNames(classs).forEach(functionName => {
+            if (excludeList.indexOf(functionName) === -1) {
+                console.log(functionName);
+            }
         });
 
         console.log("Profiler-end setup");
     }
 }
 
-interface DERP {
-    prototype: any;
-}
+const excludeList: string[] =  ["prototype", "length"];
