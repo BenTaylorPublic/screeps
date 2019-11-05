e13a827216edc713a786ae03c7e445ffb8bdb888
ben <benjaminjacktaylor@hotmail.com>
2019-11-05 15:44:25 +1100

    Console table is not a thing lol

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,6 @@ import {ProfilerWrapper} from "./profiler/profiler-wrapper";
 
 console.log("Script reloaded");
 
-const people: any = [["John", "Smith"], ["Jane", "Doe"], ["Emily", "Jones"]];
-console.table(people);
-
 setupMyMemory();
 
 ProfilerWrapper.setup();
