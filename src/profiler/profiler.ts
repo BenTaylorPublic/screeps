dd4bbbb3f40687188249a9e65e3a129b7088416a
ben <benjaminjacktaylor@hotmail.com>
2019-11-05 13:09:41 +1100

    Actually wrapping now, to see how performance is

--- src/profiler/profiler.ts
+++ src/profiler/profiler.ts
@@ -13,18 +13,31 @@ export class Profiler {
                 return; //This is a 'continue' in a forEach loop
             }
             console.log(classString + "." + functionName);
-            // const originalFunction: Function = (thing as any)[functionName];
-            // (thing as any)[functionName] = function (): any {
-            //     //TODO: Timer start here
-            //
-            //     if (arguments.length === 2) {
-            //         originalFunction.call(thing, arguments[0], arguments[1]);
-            //     } else {
-            //         console.log(arguments.length);
-            //     }
-            //
-            //     //TODO: Timer end here
-            // };
+            const originalFunction: Function = (thing as any)[functionName];
+            (thing as any)[functionName] = function (): any {
+                //TODO: Timer start here
+                if (arguments.length === 0) {
+                    originalFunction.call(thing);
+                } else if (arguments.length === 1) {
+                    originalFunction.call(thing, arguments[0]);
+                } else if (arguments.length === 2) {
+                    originalFunction.call(thing, arguments[0], arguments[1]);
+                } else if (arguments.length === 3) {
+                    originalFunction.call(thing, arguments[0], arguments[1], arguments[2]);
+                } else if (arguments.length === 4) {
+                    originalFunction.call(thing, arguments[0], arguments[1], arguments[2], arguments[3]);
+                } else if (arguments.length === 5) {
+                    originalFunction.call(thing, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+                } else if (arguments.length === 6) {
+                    originalFunction.call(thing, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5]);
+                } else if (arguments.length === 7) {
+                    originalFunction.call(thing, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6]);
+                } else {
+                    console.log("ERROR: " + arguments.length);
+                }
+
+                //TODO: Timer end here
+            };
         });
 
         console.log("Profiler-end setup");
