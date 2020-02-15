032bfc96f956ceaa74ea0981aedc9c17a6b1ff90
ben <benjaminjacktaylor@hotmail.com>
2020-02-15 17:14:07 +1100

    5.2 -> 5.4 now just needs 1 link per source, +1 for bank

--- src/room/stages/stage5_2.ts
+++ src/room/stages/stage5_2.ts
@@ -5,12 +5,12 @@ import {ReportController} from "../../reporting/report-controller";
 // tslint:disable-next-line: class-name
 export class Stage5_2 {
     /*
-    5.2 -> 5.4 : Room has 3 links
-    5.2 <- 5.4 : Room has < 3 links
+    5.2 -> 5.4 : Room has > sourceCount links
+    5.2 <- 5.4 : Room has <= sourceCount links
     */
     public static up(myRoom: MyRoom, room: Room): boolean {
         this.step(myRoom, room);
-        if (HelperFunctions.amountOfExtensions(room, STRUCTURE_LINK) >= 3) {
+        if (HelperFunctions.amountOfExtensions(room, STRUCTURE_LINK) > myRoom.mySources.length) {
             myRoom.roomStage = 5.4;
             ReportController.log("STAGE", "Room " + myRoom.name + " increased to room stage 5.4");
             return true;
@@ -19,7 +19,7 @@ export class Stage5_2 {
     }
 
     public static down(myRoom: MyRoom, room: Room): boolean {
-        if (HelperFunctions.amountOfExtensions(room, STRUCTURE_LINK) < 3) {
+        if (HelperFunctions.amountOfExtensions(room, STRUCTURE_LINK) <= myRoom.mySources.length) {
             myRoom.roomStage = 5.2;
             ReportController.log("STAGE", "Room " + myRoom.name + " decreased to room stage 5.2");
             return true;
