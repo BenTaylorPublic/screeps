d156fe0cc5e7edc7e8be6f4719c9d885a2c694ac
ben <benjaminjacktaylor@hotmail.com>
2020-02-19 17:59:17 +1100

    Reduced TTL lower limit to 1000 temporarily

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -13,7 +13,7 @@ export class Constants {
     public static CONSCRIPTION_QUICK_MINIMUM_STAGE: Stage = 4;
     public static CONSCRIPTION_PRESSURE_MINIMUM_STAGE: Stage = 6;
     public static RALLY_FLAG_RANGE: number = 3;
-    public static POWER_SCAVENGE_TTL_MIN: number = 4500;
+    public static POWER_SCAVENGE_TTL_MIN: number = 1000;
     public static POWER_SCAVENGE_RANGE_MAX: number = 10;
     public static POWER_SCAVENGE_DAMAGE_CREEP_COUNT: number = 3;
     public static POWER_SCAVENGE_ROOM_STAGE: Stage = 8;
