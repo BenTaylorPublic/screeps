6fd21b1bf0b1f7f19e80fa8eb651b25e0702c54c
ben <benjaminjacktaylor@hotmail.com>
2020-02-22 08:37:08 +1100

    Fixed silly mistake

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {ProfilerWrapper} from "./profiler/profiler-wrapper";
 import {FunctionProfiler} from "./profiler/function-profiler/function-profiler";
 
 console.log("Script reloaded");
+Memory.myMemory.empire.powerScavenge.banksScavengingFrom = [];
 
 setupMyMemory();
 
