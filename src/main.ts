0564ceb514cab78485927e1157f4e5051ac29b27
ben <benjaminjacktaylor@hotmail.com>
2020-03-03 12:22:42 +1100

    Hopefully fixing all the rooms now

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,26 @@ import {EmpireController} from "./empire/empire-controller";
 import {ScheduleController} from "./schedule/schedule-controller";
 import {ProfilerWrapper} from "./profiler/profiler-wrapper";
 import {FunctionProfiler} from "./profiler/function-profiler/function-profiler";
+import {HelperFunctions} from "./global/helper-functions";
+import {SpawnQueueController} from "./global/spawn-queue-controller";
 
 console.log("Script reloaded");
-console.log(Game.spawns["Spawn1"].spawnCreep([MOVE, CARRY], "FIXCREEP1"));
 
 for (let i: number = 0; i < Memory.myMemory.myRooms.length; i++) {
     const myRoom: MyRoom = Memory.myMemory.myRooms[i];
-    if (myRoom.name === "E16S18") {
-        myRoom.myCreeps.push({
-            name: "FIXCREEP1",
-            role: "Stocker",
-            assignedRoomName: myRoom.name,
-            spawningStatus: "spawning",
-            roomMoveTarget: {
-                pos: null,
-                path: []
-            },
-            state: "Pickup"
-        } as MyCreep);
-    }
+    const name: string = "FixCreep" + HelperFunctions.getId();
+    SpawnQueueController.queueCreepSpawn([MOVE, CARRY], myRoom, 999, name);
+    myRoom.myCreeps.push({
+        name: name,
+        role: "Stocker",
+        assignedRoomName: myRoom.name,
+        spawningStatus: "queued",
+        roomMoveTarget: {
+            pos: null,
+            path: []
+        },
+        state: "Pickup"
+    } as MyCreep);
     // for (let j: number = myRoom.myCreeps.length - 1; j >= 0; j--) {
     //     const myCreep: MyCreep = myRoom.myCreeps[j];
     // }
