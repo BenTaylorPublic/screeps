78312be9cd7f4e72ec03be5e0fcfc8d9f21bb824
ben <benjaminjacktaylor@hotmail.com>
2020-03-03 22:41:47 +1100

    Removed once off code

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,15 @@ import {FunctionProfiler} from "./profiler/function-profiler/function-profiler";
 
 console.log("Script reloaded");
 
-for (let i: number = 0; i < Memory.myMemory.myRooms.length; i++) {
-    const myRoom: MyRoom = Memory.myMemory.myRooms[i];
-    for (let j: number = myRoom.myCreeps.length - 1; j >= 0; j--) {
-        const myCreep: MyCreep = myRoom.myCreeps[j];
-        if (Game.creeps[myCreep.name] != null) {
-            Game.creeps[myCreep.name].suicide();
-        }
-    }
-    for (let j: number = 0; j < myRoom.mySources.length; j++) {
-        const mySource: MySource = myRoom.mySources[j];
-        mySource.minerName = null;
-    }
-    myRoom.bankLinkerName = null;
-    myRoom.spawnQueue = [];
-    Memory.myMemory.globalId = 1;
-}
+// for (let i: number = 0; i < Memory.myMemory.myRooms.length; i++) {
+//     const myRoom: MyRoom = Memory.myMemory.myRooms[i];
+//     for (let j: number = myRoom.myCreeps.length - 1; j >= 0; j--) {
+//         const myCreep: MyCreep = myRoom.myCreeps[j];
+//     }
+//     for (let j: number = 0; j < myRoom.mySources.length; j++) {
+//         const mySource: MySource = myRoom.mySources[j];
+//     }
+// }
 
 setupMyMemory();
 // ProfilerWrapper.setup();
