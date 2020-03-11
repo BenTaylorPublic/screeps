96f30621428bb72c93edfe344d72e31ef6b31799
ben <benjaminjacktaylor@hotmail.com>
2020-03-11 18:38:46 +1100

    Fixed 1.6->2 attention again

--- src/room/stages/stage1_6.ts
+++ src/room/stages/stage1_6.ts
@@ -103,9 +103,8 @@ export class Stage1_6 {
             }
         }
 
-        if (flagsPlaced !== myRoom.mySources.length &&
-            room.find(FIND_CONSTRUCTION_SITES).length === 0 &&
-            HelperFunctions.amountOfStructure(room, STRUCTURE_CONTAINER) < myRoom.mySources.length) {
+        if (room.find(FIND_CONSTRUCTION_SITES).length === 0 &&
+            flagsPlaced + HelperFunctions.amountOfStructure(room, STRUCTURE_CONTAINER) < myRoom.mySources.length) {
             ReportController.email("ATTENTION: Room " + HelperFunctions.roomNameAsLink(room.name) + " needs cache container flag (cont)",
                 ReportCooldownConstants.DAY);
         }
