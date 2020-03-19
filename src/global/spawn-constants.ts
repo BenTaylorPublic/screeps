60232efb37b42e1ed8c707dc6cf27d9fd3361569
ben <benjaminjacktaylor@hotmail.com>
2020-03-19 23:31:27 +1100

    Reduced scavenger priority so I can scavenge over night

--- src/global/spawn-constants.ts
+++ src/global/spawn-constants.ts
@@ -1,7 +1,6 @@
 export class SpawnConstants {
     public static ATTACK_QUICK: number = 5;
     public static FORCE_LABORER: number = 4.5;
-    public static SCAVENGER: number = 4.4;
     public static STOCKER: number = 4.25;
     public static CLAIMER: number = 4;
     public static BANK_LINKER: number = 3.5;
@@ -11,5 +10,6 @@ export class SpawnConstants {
     public static ATTACK_PRESSURE: number = 2.5;
     public static HAULER: number = 2;
     public static LABORER: number = 1;
+    public static SCAVENGER: number = 0.5;
     public static SIGNER: number = 0.5;
 }
