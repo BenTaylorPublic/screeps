919698ec9db5efc24a3409c21e31a171f2809ee4
ben <benjaminjacktaylor@hotmail.com>
2020-04-14 19:03:17 +1000

    Set mineral constants

--- src/global/constants/mineral-constants.ts
+++ src/global/constants/mineral-constants.ts
@@ -1,9 +1,29 @@
 export class MineralConstants {
     public static getT1MineralConstants(): MineralConstants {
         return {
+            Z: {
+                lower: 15_00,
+                upper: 19_000
+            },
             K: {
-                lower: 100,
-                upper: 200
+                lower: 15_00,
+                upper: 19_00
+            },
+            U: {
+                lower: 15_00,
+                upper: 19_000
+            },
+            L: {
+                lower: 15_00,
+                upper: 19_000
+            },
+            H: {
+                lower: 15_00,
+                upper: 19_000
+            },
+            O: {
+                lower: 15_00,
+                upper: 19_000
             }
         };
     }
