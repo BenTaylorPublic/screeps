baf9759e300f10592e12fdf903e14f7338f767bc
ben <benjaminjacktaylor@hotmail.com>
2020-04-14 16:58:36 +1000

    Another go

--- src/global/constants/mineral-constants.ts
+++ src/global/constants/mineral-constants.ts
@@ -2,8 +2,8 @@ export class MineralConstants {
     public static getT1MineralConstants(): MineralConstants {
         return {
             K: {
-                lower: 20000,
-                upper: 21000
+                lower: 100,
+                upper: 200
             }
         };
     }
