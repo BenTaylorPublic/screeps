ae7ed31c17b1bb651735583c9491a4f0550085dc
ben <benjaminjacktaylor@hotmail.com>
2020-04-14 22:24:24 +1000

    Changed limits

--- src/global/constants/mineral-constants.ts
+++ src/global/constants/mineral-constants.ts
@@ -2,27 +2,27 @@ export class MineralConstants {
     public static getT1MineralConstants(): MineralConstants {
         return {
             Z: {
-                lower: 15_00,
+                lower: 15_000,
                 upper: 19_000
             },
             K: {
-                lower: 15_00,
-                upper: 19_00
+                lower: 15_000,
+                upper: 19_000
             },
             U: {
-                lower: 15_00,
+                lower: 15_000,
                 upper: 19_000
             },
             L: {
-                lower: 15_00,
+                lower: 15_000,
                 upper: 19_000
             },
             H: {
-                lower: 15_00,
+                lower: 15_000,
                 upper: 19_000
             },
             O: {
-                lower: 15_00,
+                lower: 15_000,
                 upper: 19_000
             }
         };
