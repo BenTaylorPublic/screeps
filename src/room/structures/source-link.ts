6af75d34b6ab26a73c1d7240ac7e31b54c4fdba2
ben <benjaminjacktaylor@hotmail.com>
2020-04-18 22:20:19 +1000

    Source links transferring to controller links

--- src/room/structures/source-link.ts
+++ src/room/structures/source-link.ts
@@ -23,6 +23,23 @@ export class RoomSourceLinkController {
 
         if (link.store.energy >= Constants.SOURCE_LINK_TRANSFER_AT) {
             //Need to transfer energy
+
+            if (myRoom.controllerLink != null &&
+                myRoom.controllerLink.id != null) {
+                const controllerLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.controllerLink.id);
+                if (controllerLink === null) {
+                    ReportController.email("ERROR: Controller link was null when accessed by ID. Setting it to null in " + LogHelper.roomNameAsLink(myRoom.name));
+                    myRoom.controllerLink.id = null;
+                } else {
+                    if (controllerLink.store.getFreeCapacity() >= link.store.energy) {
+                        if (link.transferEnergy(controllerLink) === OK) {
+                            return;
+                        }
+                    }
+                }
+
+            }
+
             const bankLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.bank.bankLink.id);
             if (bankLink === null) {
                 ReportController.email("ERROR: Bank link was null when accessed by ID. Setting it to null in " + LogHelper.roomNameAsLink(myRoom.name));
