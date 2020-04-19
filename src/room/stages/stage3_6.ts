0a3642386083eda5c45f187c19dedfb9f3e0cf66
ben <benjaminjacktaylor@hotmail.com>
2020-04-19 21:49:46 +1000

    Fixing the bumpy stage 3.6 -> 4

--- src/room/stages/stage3_6.ts
+++ src/room/stages/stage3_6.ts
@@ -12,26 +12,18 @@ export class Stage3_6 {
     */
     public static up(myRoom: MyRoom, room: Room): boolean {
         this.step(myRoom, room);
-        if (RoomHelper.amountOfStructure(room, STRUCTURE_STORAGE) >= 1) {
+        if (RoomHelper.amountOfStructure(room, STRUCTURE_STORAGE) >= 1 &&
+            myRoom.bank != null) {
             myRoom.roomStage = 4;
             ReportController.email("STAGE+ 4 " + LogHelper.roomNameAsLink(myRoom.name) + " bank");
-
-            //Setting the bank object to null, so it's usable for the rest of the tick
-            const bankPos: RoomPosition = RoomHelper.myPosToRoomPos((myRoom.bank as Bank).bankPos);
-            const structures: Structure<StructureConstant>[] = bankPos.lookFor(LOOK_STRUCTURES);
-            for (let j = 0; j < structures.length; j++) {
-                if (structures[j].structureType === STRUCTURE_STORAGE) {
-                    (myRoom.bank as Bank).object = structures[j] as StructureStorage;
-                    break;
-                }
-            }
             return true;
         }
         return false;
     }
 
     public static down(myRoom: MyRoom, room: Room): boolean {
-        if (RoomHelper.amountOfStructure(room, STRUCTURE_STORAGE) < 1) {
+        if (RoomHelper.amountOfStructure(room, STRUCTURE_STORAGE) < 1 ||
+            myRoom.bank == null) {
             myRoom.roomStage = 3.6;
             ReportController.email("STAGE- 3.6 " + LogHelper.roomNameAsLink(myRoom.name) + " bank");
             return true;
@@ -40,6 +32,18 @@ export class Stage3_6 {
     }
 
     private static step(myRoom: MyRoom, room: Room): void {
+        const storageFlag: Flag | null = FlagHelper.getFlag(["storage"], myRoom.name);
+        let placedBank: boolean = false;
+        if (storageFlag != null) {
+            const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(storageFlag.pos, STRUCTURE_STORAGE);
+            if (result === OK) {
+                ReportController.log("Placed storage bank construction site in " + LogHelper.roomNameAsLink(room.name));
+                storageFlag.remove();
+                placedBank = true;
+            } else {
+                ReportController.email("ERROR: Placing a storage bank construction site errored in " + LogHelper.roomNameAsLink(room.name));
+            }
+        }
         const bankLinkerPosFlag: Flag | null = FlagHelper.getFlag(["bank", "linker", "pos"], myRoom.name);
         if (bankLinkerPosFlag == null) {
             return;
@@ -60,21 +64,9 @@ export class Stage3_6 {
                 bankLinkerName: null,
                 bankLinkerPos: RoomHelper.roomPosToMyPos(bankLinkerPosFlag.pos),
                 bankLink: null,
-                object: null
+                object: storage[0]
             };
             return;
-        }
-        const flag: Flag | null = FlagHelper.getFlag(["storage"], myRoom.name);
-        let placedBank: boolean = false;
-        if (flag != null) {
-            const result: ScreepsReturnCode = Game.rooms[myRoom.name].createConstructionSite(flag.pos, STRUCTURE_STORAGE);
-            if (result === OK) {
-                ReportController.log("Placed storage bank construction site in " + LogHelper.roomNameAsLink(room.name));
-                flag.remove();
-                placedBank = true;
-            } else {
-                ReportController.email("ERROR: Placing a storage bank construction site errored in " + LogHelper.roomNameAsLink(room.name));
-            }
         }
 
         if (Game.rooms[myRoom.name].find(FIND_CONSTRUCTION_SITES).length === 0 &&
