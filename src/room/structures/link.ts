d6a9ca0238613ca23fe623ceaa00a66657ab466d
ben <benjaminjacktaylor@hotmail.com>
2020-04-19 13:38:54 +1000

    Bank link will transfer to controller link if needed

--- src/room/structures/link.ts
+++ src/room/structures/link.ts
@@ -5,6 +5,13 @@ import {ReportController} from "../../reporting/report-controller";
 
 export class RoomLinkController {
     public static run(myRoom: MyRoom): boolean {
+        if (myRoom.roomStage < 4 ||
+            myRoom.bank == null ||
+            myRoom.bank.bankLink == null ||
+            myRoom.bank.bankLink.id == null) {
+            return false;
+        }
+
         let linkControllerNeedsEnergy: boolean = false;
         let controllerLink: StructureLink | null = null;
         if (myRoom.controllerLink != null &&
@@ -13,7 +20,7 @@ export class RoomLinkController {
             if (controllerLink == null) {
                 ReportController.email("ERROR: Controller link was null when accessed by ID. Setting it to null in " + LogHelper.roomNameAsLink(myRoom.name));
                 myRoom.controllerLink.id = null;
-            } else if (controllerLink.store.getFreeCapacity(RESOURCE_ENERGY) >= Constants.SOURCE_LINK_TRANSFER_AT) {
+            } else if (controllerLink.store.getFreeCapacity(RESOURCE_ENERGY) >= Constants.LINK_TRANSFER_AT) {
                 linkControllerNeedsEnergy = true;
             } else {
                 controllerLink = null;
@@ -27,6 +34,22 @@ export class RoomLinkController {
                     linkControllerNeedsEnergy = false;
                 }
             }
+        }
+
+        if (linkControllerNeedsEnergy) {
+            const bankLink: StructureLink | null = Game.getObjectById<StructureLink>(myRoom.bank.bankLink.id);
+            if (bankLink == null) {
+                ReportController.email("ERROR: Bank link was null when accessed by ID. Setting it to null in " + LogHelper.roomNameAsLink(myRoom.name));
+                myRoom.bank.bankLink.id = null;
+            } else if (bankLink.store.energy >= Constants.LINK_TRANSFER_AT &&
+                (controllerLink as StructureLink).store.getFreeCapacity(RESOURCE_ENERGY) >= bankLink.store.energy) {
+                if (bankLink.transferEnergy(controllerLink as StructureLink) === OK) {
+                    linkControllerNeedsEnergy = false;
+                }
+
+            }
+
+
         }
         return linkControllerNeedsEnergy;
     }
@@ -48,7 +71,7 @@ export class RoomLinkController {
             return false;
         }
 
-        if (link.store.energy >= Constants.SOURCE_LINK_TRANSFER_AT) {
+        if (link.store.energy >= Constants.LINK_TRANSFER_AT) {
             //Need to transfer energy
             if (controllerLink != null &&
                 controllerLink.store.getFreeCapacity(RESOURCE_ENERGY) >= link.store.energy) {
