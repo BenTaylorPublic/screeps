0e6b442a68852299112f02457bddc92508c20fcb
ben <benjaminjacktaylor@hotmail.com>
2020-10-04 00:38:15 +1000

    Getting legolas onto a flag

--- src/empire/role/legolas.ts
+++ src/empire/role/legolas.ts
@@ -1,4 +1,7 @@
 import {CreepHelper} from "../../global/helpers/creep-helper";
+import {FlagHelper} from "../../global/helpers/flag-helper";
+import {RoomHelper} from "../../global/helpers/room-helper";
+import {MovementHelper} from "../../global/helpers/movement-helper";
 
 export class RoleLegolas {
     public static run(legolas: Legolas): void {
@@ -7,6 +10,13 @@ export class RoleLegolas {
         }
 
         const creep: Creep = Game.creeps[legolas.name];
-        console.log("TODO: " + creep.name);
+
+        const standOnFlag: Flag | null = FlagHelper.getFlag1(["test", "stand", "on"], creep.room.name);
+        if (standOnFlag != null &&
+            !RoomHelper.posMatches3(creep.pos, standOnFlag.pos)) {
+            MovementHelper.myMoveTo(creep, standOnFlag.pos, legolas);
+        } else {
+            return;
+        }
     }
 }
